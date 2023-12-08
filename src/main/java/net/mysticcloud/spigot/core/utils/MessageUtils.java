dc50781ea5df062a064a612a47c7a64176159f1b
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-12-07 21:44:56 -0600

    placeholder utils & symbols  \#release

--- src/main/java/net/mysticcloud/spigot/core/utils/MessageUtils.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/MessageUtils.java
@@ -5,6 +5,7 @@ import com.google.common.io.ByteStreams;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -30,6 +31,43 @@ public class MessageUtils {
 
     public static void prefixes(String key, String prefix) {
         prefixes.put(key, colorize(prefix));
+    }
+
+    public static String fade(String fromHex, String toHex, String string) {
+        int[] start = getRGB(fromHex);
+        int[] last = getRGB(toHex);
+
+        StringBuilder sb = new StringBuilder();
+
+        Integer dR = numberFade(start[0], last[0], string.length());
+        Integer dG = numberFade(start[1], last[1], string.length());
+        Integer dB = numberFade(start[2], last[2], string.length());
+
+        for (int i = 0; i < string.length(); i++) {
+            Color c = new Color(start[0] + dR * i, start[1] + dG * i, start[2] + dB * i);
+
+            sb.append(net.md_5.bungee.api.ChatColor.of(c) + "" + string.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    private static int[] getRGB(String rgb) {
+        int[] ret = new int[3];
+        for (int i = 0; i < 3; i++) {
+            ret[i] = hexToInt(rgb.charAt(i * 2), rgb.charAt(i * 2 + 1));
+        }
+        return ret;
+    }
+
+    private static int hexToInt(char a, char b) {
+        int x = a < 65 ? a - 48 : a - 55;
+        int y = b < 65 ? b - 48 : b - 55;
+        return x * 16 + y;
+    }
+
+    private static Integer numberFade(int i, int f, int n) {
+        int d = (f - i) / (n - 1);
+        return d;
     }
 
 
