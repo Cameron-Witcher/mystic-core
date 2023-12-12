9a49446ae308d6de2f77890c040c942238f8ab55
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-12-12 03:47:34 -0600

    SQL Utils  \#release

--- src/main/java/net/mysticcloud/spigot/core/utils/accounts/MysticPlayer.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/accounts/MysticPlayer.java
@@ -17,4 +17,19 @@ public class MysticPlayer {
     public JSONObject getData(){
         return data;
     }
+
+    public void putData(String key, Object value){
+        data.put(key, value);
+    }
+
+    public Object getData(String key){
+        return data.has(key) ? data.get(key) : null;
+    }
+    public int getInt(String key){
+        return data.has(key) ? data.getInt(key) : 0;
+    }
+    public String getString(String key){
+        return data.has(key) ? data.getString(key) : "";
+    }
+
 }
