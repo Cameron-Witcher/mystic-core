03525e573bc56a1fa28bfd162220b54685e2899b
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-12-12 03:51:04 -0600

    SQL Utils  \#release

--- src/main/java/net/mysticcloud/spigot/core/MysticCore.java
+++ src/main/java/net/mysticcloud/spigot/core/MysticCore.java
@@ -6,6 +6,7 @@ import net.mysticcloud.spigot.core.listeners.InteractionListener;
 import net.mysticcloud.spigot.core.listeners.ServerListener;
 import net.mysticcloud.spigot.core.listeners.channels.MessageListener;
 import net.mysticcloud.spigot.core.utils.CoreUtils;
+import net.mysticcloud.spigot.core.utils.accounts.AccountManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,6 +25,12 @@ public class MysticCore extends JavaPlugin {
         new ServerListener(this);
 
         new AdminCommands(this, "update", "kick", "region", "sudo");
+
+
+        //For reloads
+        for(Player player : Bukkit.getOnlinePlayers()){
+            AccountManager.getMysticPlayer(player.getUniqueId());
+        }
     }
 
     public void onDisable() {
