2b1d0df37db38db3d35dd0e682cdc8c238bd75cb
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-12-16 14:55:02 -0600

    R tag broke  \#release

--- src/main/java/net/mysticcloud/spigot/core/utils/CoreUtils.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/CoreUtils.java
@@ -142,10 +142,13 @@ public class CoreUtils {
     }
 
     public static Location decryptLocation(String s) {
+        Bukkit.broadcastMessage("1: " + s);
         if (s.startsWith("location:")) s = s.replaceAll("location:", "");
-
+        Bukkit.broadcastMessage("2: " + s);
         if (s.contains(",")) s = s.replaceAll(",", ".");
+        Bukkit.broadcastMessage("3: " + s);
         String[] args = s.split(":");
+        Bukkit.broadcastMessage("Size: " + args.length);
         Location r = new Location(Bukkit.getWorld(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3]));
         if (args.length >= 5) {
             r.setPitch(Float.parseFloat(args[4]));
